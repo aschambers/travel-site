82e7174b0ea4e10b2f36486617da3fcad278d277
aschambers <aschambers4291@gmail.com>
2018-03-10 22:19:55 -0800

    work on making text mobile first

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
 	// console.log('watching stylesheet changes');
 	// move water in pipe from point a to b, and manipulate
 	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+	.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
 	.on('error', function(errorInfo) {
 		console.log(errorInfo.toString());
 		this.emit('end');
