2addc524284668b8cc68faadf6c41bd0e622d8ea
aschambers <aschambers4291@gmail.com>
2018-03-10 21:35:12 -0800

    split gulp commands into styles and watch files

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+
+gulp.task('styles', function() {
+	// console.log('watching stylesheet changes');
+	// move water in pipe from point a to b, and manipulate
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+// browser sync create method to autorefresh when saving a file
+var browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	});
+
+	watch('./app/index.html', function() {
+		// gulp.start('html');
+		browserSync.reload();
+	});
+
+	watch('./app/assets/styles/**/*.css', function() {
+		// gulp.start('styles');
+		gulp.start('cssInject');
+	});
+});
+
+// anytime we save a change to any css file, we are triggering the css inject task
+// we built the css inject task so that it won't begin until css styles file will compile 
+// and complete first
+gulp.task('cssInject', ['styles'], function() {
+	// method to make available to the browser
+	return gulp.src("./app/temp/styles/styles.css")
+	.pipe(browserSync.stream());
+});
